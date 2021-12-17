1c000f26f04c8dc63b164299c9a8a1e34cefdf46
Julia Cox <juliacox525@gmail.com>
2021-12-17 12:34:29 -0500

    First commit

---
+++ AR43.R
@@ -0,0 +1,112 @@
+#Calculate EtOH fish biomass using length weight relationships from literature. 
+#Fish ID'ed to family
+#SL measured in ImageJ
+#Use frozen fish IDs to choose which references to use in analysis 
+#
+#Cruise: AR43
+#Calculating weighted means for a and b values: 
+#Exclude frozen fish only ID'd to family, unless family 
+#is the only ID available for that family (i.e. Alepocephalidae)
+#If a reference is not available at the genus level, the genus will be excluded
+#altogether. 
+#
+#Calulate % of genera within each family. 
+#Match frozen fish genera with WW a and b values from literature.
+#Make sure units are compatable, all measurements should be converted 
+#to mm and mg. Adjust a value accordingly (b value is not affected).  
+#
+#Calulate weighted means based on genus % of each family.
+#Apply calculated a and b values to EtOH fish lengths in
+#equation y = ax^b
+
+setwd("/Users/juliacox/Desktop/OTZ Fish Biomass/Data")
+library(readxl)
+library(tidyverse)
+library(dplyr)
+#import data
+lit.rev <- read_excel("Collated Length Weight relationships.xlsx")
+froz.fish.WA <- read_excel("Julia_RegionalFishCounts_4biomass.xlsx", sheet = "W. Atlantic")
+AR43.data <- read_excel("AR43 Whole Lengths.xlsx")
+#narrow down families in froz.fish.WA to ones represented in AR43.data
+froz.fish.WA <- rename(froz.fish.WA, Family = family)
+froz.fish.use <- subset(froz.fish.WA, (Family %in% AR43.data$Family))
+#separate "lowest_taxa" col. in froz.fish.use into genus and species cols. 
+froz.fish.WA.separated <-separate(froz.fish.use, lowest_taxa, c("genus","species"), sep = " ") %>% 
+  replace_na(list(species = "sp."))
+
+#remove rows only identified to family, unless there are no members of family ID'd to genus  
+froz.fish.WA.separated$use <- ifelse(froz.fish.WA.separated$genus == froz.fish.WA.separated$Family,
+                                     !duplicated(froz.fish.WA.separated$Family),
+                                     froz.fish.WA.separated$genus)
+froz.fish.WA.use <- froz.fish.WA.separated[!grepl("FALSE", froz.fish.WA.separated$use),]
+
+#make sure all units are in mm and mg by converting a values 
+#convert all a values so y unit = mg 
+lit.rev$a_value_mg<-ifelse(lit.rev$`Weight unit`== "g",
+                           lit.rev$`a value (WW)`*1000, 
+                           lit.rev$`a value (WW)`) 
+
+#convert all a values so x unit = mm - does this work? 
+lit.rev$ycm <- lit.rev$a_value_mg*(10^lit.rev$`b value (WW)`)
+
+lit.rev$a_corrected <- ifelse(lit.rev$`Length unit`== "cm",
+                              lit.rev$ycm/(100^lit.rev$`b value (WW)`), 
+                              lit.rev$a_value_mg) 
+#remove spp. measured in TL
+lit.rev.use <- lit.rev[!grepl("TL", lit.rev$`Length type`),]
+
+#take genus a and b means of lit.rev.use
+lit.rev.separated <-separate(lit.rev.use, `Species`, c("genus","species"), sep = " ") %>% 
+  replace_na(list(species = NA))
+#since we don't have a genus for Alepocephalidae, insert family name in genus spot 
+#for lit.rev
+lit.rev.final <- lit.rev.separated %>%
+  mutate(genus = replace(genus, genus == "Alepocephalus", Family[genus=="Alepocephalus"]))
+
+genus.a.mean <- tapply(lit.rev.final$a_corrected,lit.rev.final$genus,mean)
+genus.a.mean.df <- as.data.frame.table(genus.a.mean)
+colnames(genus.a.mean.df) <- c("genus", "a_mean")
+genus.b.mean <- tapply(lit.rev.final$`b value (WW)`, lit.rev.final$genus,mean)
+genus.b.mean.df <- as.data.frame.table(genus.b.mean)
+colnames(genus.b.mean.df) <- c("genus", "b_mean")
+
+#get genus totals for froz.fish.WA.use
+genus.totals <- data.frame(aggregate(Count~Family+genus, data = froz.fish.WA.use, FUN = sum)) 
+
+#find which genera are missing in genus.a.mean.df and genus.b.mean.df 
+missing.genera <- subset(genus.totals,!(genus%in%genus.a.mean.df$genus))
+sum(missing.genera$Count)/sum(genus.totals$Count)*100
+#2.8% of individuals missing in missing genera 
+#use missing.genera list to search for missing genera 
+
+#combine genus.totals and lit.rev dataframes by $genus
+total <- Reduce(merge, list(genus.totals, genus.a.mean.df, genus.b.mean.df))
+#get summary statistics for a and b 
+library(data.table)
+long <- melt(total(wide), id.vars = c("Code","Country"), variable.name = "year")
+reshape(total, idvar = "genus", timevar = "a_mean", direction = "wide")
+
+#take weighted means for a and b 
+#doesn't work 
+family.totals <-data.frame(aggregate(Count~Family, data = total, FUN = sum))
+colnames(family.totals) <- c("Family", "total_count_fam")
+total <- merge(total, family.totals, by.x="Family", by.y="Family")
+total$proportion_fam <- total$Count/total$total_count
+total$proportion_a <- total$a_mean*total$proportion_fam
+total$proportion_b <- total$b_mean*total$proportion_fam
+
+a <- data.frame(aggregate(proportion_a~Family, data = total, FUN = sum)) 
+b <- aggregate(proportion_b~Family, data = total, FUN = sum)
+weighted.growth.fact <- merge(a, b)
+
+#apply a and b values to measured AR43 fish 
+AR43.data <- read_excel("AR43 Whole Lengths.xlsx")
+AR43.gf <- merge(AR43.data, weighted.growth.fact, by.x="Family", by.y="Family")
+AR43.gf$`Length (mm)` <- AR43.gf$`Length (cm)`*10
+AR43.gf$`Weight (mg)` <- AR43.gf$proportion_a*(AR43.gf$`Length (mm)`^AR43.gf$proportion_b)
+#test#
+sd(total$a_mean %in% total$Family)
+#vv sandbox vv
+#helpful code for sp. and spp. 
+#https://github.com/marytoner/WoRMS_matching/blob/master/demo_modified_forLaurenM.Rmd#L56
+
